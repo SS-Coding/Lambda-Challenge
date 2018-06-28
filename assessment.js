973d48403ed650ec83cee9fdf9f4f25e6b2c26c1
SS-Coding <40672531+SS-Coding@users.noreply.github.com>
2018-06-28 15:09:33 -0500

    Completed assessment

--- assessment.js
+++ assessment.js
@@ -1,13 +1,13 @@
 /*
 Work through the problems in this file.  As you work through each problem periodically run: npm test
 Your function name and the string must match the instructions exactly otherwise the tests will fail.
-After writing your function uncomment the matching function reference at the bottom of the file.
-*/
+After writing your function uncomment the matching function reference at the bottom of the file.*/
 
 // 1. Write a function called helloWorld that returns the string 'Hello World!'.
 
 function helloWorld() {
-
+ 
+ return('Hello World!');
 }
 
 /*
@@ -17,28 +17,41 @@ function helloWorld() {
      If num is divisible by 5 return the string 'School'
      If num is divisible by 3 AND 5 return the string 'Lambda School' (notice the space)
      If num is NOT divisible by 3 or 5 then return num.
-
      Example:
              lambdaSchool(15); // returns 'Lambda School'
              lambdaSchool(8); // returns 8
 */
 
-function lambdaSchool() {
-
+function lambdaSchool(num) {
+  if (num % 5 === 0 && num % 3 === 0)
+  return "Lambda School";
+  else if (num % 5 === 0)
+  return "School";
+  else if (num % 3 === 0)
+  return "Lambda";
+  return num;
 }
+  
+  
 
 /*
 3. Write a function called longestString that has a single parameter called strs.
      strs will be an array of strings.
      Return the longest string that is in strs.
      If there is a tie for the longest string then return the one that occurs first.
-
      Example:
              longestString(['hi', 'hello', 'ni hao', 'guten tag']); // returns 'guten tag'
              longestString(['JavaScript', 'HTML', 'CSS']); // returns 'JavaScript'
 */
 
-function longestString() {
+function longestString(str) {
+  longestStr= "";
+  for (let i = 0; i < str.length; i++){
+    let element = str[i];
+    if (element.length > longestStr.length)
+    longestStr = element;
+  }
+  return longestStr;
 
 }
 
@@ -48,7 +61,6 @@ function longestString() {
      Each user object has a property called age that is a number.
      Compute the average age of all user objects in the users array.
      Round off the decimals if needed and return the number.
-
      Example:
              const users = [{
                name: 'Brendan Eich',
@@ -63,13 +75,13 @@ function longestString() {
              computeUserAverageAge(users); // returns 62 (This number is rounded up from 61.6666)
 */
 
-function computeUserAverageAge() {
+function computeUserAverageAge(users) {
+totalAge = 0;
+for (let i = 0; i < users.length; i++) {
+   let element = users[i];
+totalAge += element.age;
+  
+}
+return Math.round(totalAge / users.length);
 
 }
-
-module.exports = {
- helloWorld,
- lambdaSchool,
- longestString,
- computeUserAverageAge
-};
